10ea83cf4308c4f9589eca4e0203c3cb2f76e640
JDarkGreen <jgomez.4net@gmail.com>
2016-09-02 18:47:33 -0500

    actulizar

--- themes/custom_theme/email/enviar.php
+++ themes/custom_theme/email/enviar.php
@@ -11,26 +11,25 @@
 
 
 	//Email A quien se le rinde cuentas
-	$webmaster_email1 = "";
+	#$webmaster_email1 = "jgomez@ingenioart.com";
 	$webmaster_email2 = "ventas@maderaliaperu.com";
 
 	include("class.phpmailer.php");
  	include("class.smtp.php");
 
 	$mail = new PHPMailer();
-	$mail->CharSet = 'UTF-8';
 
-	/*$mail->IsSMTP(); // send via SMTP
+	$mail->IsSMTP(); // send via SMTP
 	$mail->SMTPSecure = 'ssl'; 
 	$mail->Host      = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	$mail->Port      = 465;
 	$mail->SMTPAuth  = true; // turn on SMTP authentication
-	$mail->Username  = "jgomez.4net@gmail.com"; // Enter your SMTP username
-	$mail->Password  = ""; // SMTP password */
+	$mail->Username  = "ventas@maderaliaperu.com"; // Enter your SMTP username
+	$mail->Password  = "madera2015"; // SMTP password */
 
 	$mail->From     = $from;
 	$mail->FromName = $name;
-	$mail->AddAddress( $webmaster_email1 );
+	#$mail->AddAddress( $webmaster_email1 );
 	$mail->AddAddress( $webmaster_email2 );
 
 	$mail->IsHTML(true); // send as HTML
